5e1687528ce9df9aea319a99bb429c55668b2d3f
Sönke Liebau <soenke.liebau@gmail.com>
2021-09-09 17:38:30 +0200

    next try - replaced more paths..

--- packaging/buildrpm.sh
+++ packaging/buildrpm.sh
@@ -89,7 +89,7 @@ echo Running copy_assets.py in $(pwd)
 
 echo Tarring
 pushd target/rpm/SOURCES
-tar czvf ${BINARY_FILE_NAME}-${PACKAGE_VERSION}.tar.gz ${PACKAGE_NAME}-${PACKAGE_VERSION}
+tar czvf ${BINARY_FILE_NAME}-${PACKAGE_VERSION}.tar.gz ${BINARY_FILE_NAME}-${PACKAGE_VERSION}
 popd
 
 echo Running rpmbuild
