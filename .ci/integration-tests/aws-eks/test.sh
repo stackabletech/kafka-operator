8fc4263bf24d7e10fea366741e898e9768f5767f
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-22 16:08:51 +0000

    removed CI files (#419)
    # Description
    
    Removed CI files

--- .ci/integration-tests/aws-eks/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone -b "$GIT_BRANCH" https://github.com/stackabletech/kafka-operator.git
-(cd kafka-operator/ && ./scripts/run_tests.sh --parallel 1)
-exit_code=$?
-./operator-logs.sh kafka > /target/kafka-operator.log
-exit $exit_code
