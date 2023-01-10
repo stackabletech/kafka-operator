13cbf64fe67a80cc94d0867554f7f54deb948c1d
Malte Sander <malte.sander.it@gmail.com>
2023-01-10 12:03:27 +0100

    removed deploy crd folder

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,11 +1,3 @@
-use stackable_kafka_crd::KafkaCluster;
-use stackable_operator::crd::CustomResourceExt;
-use stackable_operator::error::OperatorResult;
-
-fn main() -> OperatorResult<()> {
+fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    KafkaCluster::write_yaml_schema("../../deploy/crd/kafkacluster.crd.yaml")?;
-
-    Ok(())
 }
