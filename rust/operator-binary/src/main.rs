67c6e16daee980e4c7d4aa8094d3107fc3a305e7
Nick <NickLarsenNZ@users.noreply.github.com>
2023-11-15 18:08:55 +0100

    refactor: combine the operator lib and binary crates (#638)

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -1,10 +1,13 @@
 use std::sync::Arc;
 
+use clap::{crate_description, crate_version, Parser};
 use futures::StreamExt;
 use product_config::ProductConfigManager;
-use stackable_kafka_crd::{KafkaCluster, OPERATOR_NAME};
+use stackable_kafka_crd::{KafkaCluster, APP_NAME, OPERATOR_NAME};
 use stackable_operator::{
-    client::Client,
+    cli::{Command, ProductOperatorRun},
+    client::{self, Client},
+    error,
     k8s_openapi::api::{
         apps::v1::StatefulSet,
         core::v1::{ConfigMap, Pod, Service, ServiceAccount},
@@ -13,6 +16,7 @@ use stackable_operator::{
     kube::runtime::{watcher, Controller},
     logging::controller::report_controller_reconciled,
     namespace::WatchNamespace,
+    CustomResourceExt,
 };
 
 use crate::{
@@ -27,7 +31,67 @@ mod product_logging;
 mod utils;
 
 mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+    pub const TARGET: Option<&str> = option_env!("TARGET");
     pub const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+}
+
+#[derive(clap::Parser)]
+#[clap(about, author)]
+struct Opts {
+    #[clap(subcommand)]
+    cmd: Command<KafkaRun>,
+}
+
+#[derive(clap::Parser)]
+struct KafkaRun {
+    #[clap(long, env)]
+    kafka_broker_clusterrole: String,
+    #[clap(flatten)]
+    common: ProductOperatorRun,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), error::Error> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        Command::Crd => KafkaCluster::print_yaml_schema()?,
+        Command::Run(KafkaRun {
+            kafka_broker_clusterrole,
+            common:
+                ProductOperatorRun {
+                    product_config,
+                    watch_namespace,
+                    tracing_target,
+                },
+        }) => {
+            stackable_operator::logging::initialize_logging(
+                "KAFKA_OPERATOR_LOG",
+                APP_NAME,
+                tracing_target,
+            );
+            stackable_operator::utils::print_startup_string(
+                crate_description!(),
+                crate_version!(),
+                built_info::GIT_VERSION,
+                built_info::TARGET.unwrap_or("unknown target"),
+                built_info::BUILT_TIME_UTC,
+                built_info::RUSTC_VERSION,
+            );
+            let controller_config = ControllerConfig {
+                broker_clusterrole: kafka_broker_clusterrole,
+            };
+            let product_config = product_config.load(&[
+                "deploy/config-spec/properties.yaml",
+                "/etc/stackable/kafka-operator/config-spec/properties.yaml",
+            ])?;
+            let client = client::create_client(Some(OPERATOR_NAME.to_string())).await?;
+            create_controller(client, controller_config, product_config, watch_namespace).await;
+        }
+    };
+
+    Ok(())
 }
 
 pub struct ControllerConfig {
