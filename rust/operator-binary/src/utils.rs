46b336915085793de0380775cbf8ce4e639a03bd
Stacky McStackface <95074132+stackable-bot@users.noreply.github.com>
2024-05-08 17:31:42 +0200

    Update templated files to rev 35a2ccd (#708)
    * Generated commit to update templated files based on rev 35a2ccd in stackabletech/operator-templating repo.
    
    Triggered by:
    Manual run triggered by: siegfriedweber with message [Upgrade all dependencies and the renovate configuration]
    
    * Apply pre-commit hooks and fix clippy warnings
    
    ---------
    
    Co-authored-by: Siegfried Weber <mail@siegfriedweber.net>

--- rust/operator-binary/src/utils.rs
+++ rust/operator-binary/src/utils.rs
@@ -1,43 +1,5 @@
-use snafu::Snafu;
 use stackable_kafka_crd::{KafkaCluster, APP_NAME, OPERATOR_NAME};
-use stackable_operator::{
-    kube::{core::DynamicObject, runtime::reflector::ObjectRef, Resource},
-    kvp::ObjectLabels,
-};
-
-#[derive(Debug, Snafu)]
-#[snafu(display(
-    "cannot build a local reference to {} from {}, because it is in another namespace",
-    obj,
-    peer
-))]
-pub struct NamespaceMismatchError {
-    obj: ObjectRef<DynamicObject>,
-    peer: ObjectRef<DynamicObject>,
-}
-pub trait ObjectRefExt {
-    fn name_in_ns_of<'a, KPeer: Resource<DynamicType = ()>>(
-        &'a self,
-        peer_obj: &KPeer,
-    ) -> Result<&'a str, NamespaceMismatchError>;
-}
-
-impl<K: Resource<DynamicType = ()>> ObjectRefExt for ObjectRef<K> {
-    fn name_in_ns_of<KPeer: Resource<DynamicType = ()>>(
-        &self,
-        peer_obj: &KPeer,
-    ) -> Result<&str, NamespaceMismatchError> {
-        if self.namespace == peer_obj.meta().namespace {
-            Ok(&self.name)
-        } else {
-            NamespaceMismatchSnafu {
-                obj: self.clone().erase(),
-                peer: ObjectRef::from_obj(peer_obj).erase(),
-            }
-            .fail()
-        }
-    }
-}
+use stackable_operator::kvp::ObjectLabels;
 
 /// Build recommended values for labels
 pub fn build_recommended_labels<'a>(
