2e953db396df21702c8ebaac367975bf4e5bf89b
Nick Larsen <nick.larsen@stackable.tech>
2025-07-23 11:16:04 +0200

    chore: Release 25.7.0
    Signed-off-by: Nick Larsen <nick.larsen@stackable.tech>

--- Cargo.nix
+++ Cargo.nix
@@ -8383,7 +8383,7 @@ rec {
       };
       "stackable-kafka-operator" = rec {
         crateName = "stackable-kafka-operator";
-        version = "25.7.0-rc1";
+        version = "25.7.0";
         edition = "2021";
         crateBin = [
           {
